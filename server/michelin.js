2bf5aa437baae48b375e04c9c60b9d6abff2564b
unknown <laurene.vaugrante@gmail.com>
2020-01-28 11:31:40 +0100

    michelin: get attributes of 1 restaurant

--- server/michelin.js
+++ server/michelin.js
@@ -10,8 +10,13 @@ const parse = data => {
   const $ = cheerio.load(data);
   const name = $('.section-main h2.restaurant-details__heading--title').text();
   const experience = $('#experience-section > ul > li:nth-child(2)').text();
-
-  return {name, experience};
+  const phone = $('body > main > div.restaurant-details > div.container > div > div.col-xl-8.col-lg-7 > section:nth-child(3) > div.row > div:nth-child(1) > div > div:nth-child(1) > div > div > a').attr('href');
+  const address = $('body > main > div.restaurant-details > div.container > div > div.col-xl-4.order-xl-8.col-lg-5.order-lg-7.restaurant-details__aside > div.restaurant-details__heading.d-lg-none > ul > li:nth-child(1)').text();
+  const price_and_type =$('body > main > div.restaurant-details > div.container > div > div.col-xl-8.col-lg-7 > section.section.section-main.restaurant-details__main > div.restaurant-details__heading.d-none.d-lg-block > ul > li.restaurant-details__heading-price').text();
+  const review =$('body > main > div.restaurant-details > div.container > div > div.col-xl-8.col-lg-7 > section.section.section-main.restaurant-details__main > div.restaurant-details__description > div.restaurant-details__description--text.js-show-description').text();
+  //var result_price = price_and_type.filter(text => text="\n"); // removing the \n
+  
+  return {name, experience, address, phone, price_and_type, review};
 };
 
 /**
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,13 +1,18 @@
 /* eslint-disable no-console, no-process-exit */
 const michelin = require('./michelin');
+const url_liste_restaurants='https://guide.michelin.com/fr/fr/ile-de-france/restaurants/bib-gourmand';
+const url_restaurant_lacroixblanche='https://guide.michelin.com/fr/fr/centre-val-de-loire/veuves/restaurant/l-auberge-de-la-croix-blanche';
 
-async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/centre-val-de-loire/veuves/restaurant/l-auberge-de-la-croix-blanche') {
+
+
+async function sandbox (searchLink = url_restaurant_lacroixblanche) {
   try {
     console.log(`🕵️‍♀️  browsing ${searchLink} source`);
 
     const restaurant = await michelin.scrapeRestaurant(searchLink);
 
     console.log(restaurant);
+    console.log(restaurant.name);
     console.log('done');
     process.exit(0);
   } catch (e) {
@@ -19,3 +24,6 @@ async function sandbox (searchLink = 'https://guide.michelin.com/fr/fr/centre-va
 const [,, searchLink] = process.argv;
 
 sandbox(searchLink);
+
+
+
